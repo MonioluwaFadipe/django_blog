39c3df1c66f91c1a2f775661ba374023a6f9aa99
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-10-11 00:32:49 +0100

    Create about route

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Home Blog</h1>')
+
+#create about page
+def about(request):
+    return HttpResponse('<h1>About Blog</h1>')
