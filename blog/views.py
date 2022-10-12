a193f39f8d11d6eb724072fb8a786024063688e7
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-10-12 17:11:19 +0100

    Add home and about templates

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home Blog</h1>')
+    return render(request, 'blog/home.html')
 
 #create about page
 def about(request):
-    return HttpResponse('<h1>About Blog</h1>')
+    return HttpResponse('<h1>About Blog</h1>') 
