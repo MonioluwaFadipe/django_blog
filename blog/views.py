7bb964d1fc289cd183594dd95c5838f9168fb612
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-10-13 10:21:43 +0100

    add posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'FadipeMD',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 13, 2022'
+    },
+    {
+        'author': 'OniET',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 15, 2022'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 #create about page
 def about(request):
