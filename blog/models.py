3876a75a29cdf9f507bf3b525e0aa3a1d16a0536
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-10-21 18:12:25 +0100

    Update database

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
     
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'FadipeMD',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 13, 2022'
-    },
-    {
-        'author': 'OniET',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 15, 2022'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
