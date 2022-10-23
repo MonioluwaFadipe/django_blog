1b02ceec5d0b7b5bac89f27c8865d5b023d43486
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-10-23 16:04:08 +0100

    Validate POST request from form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from re import L
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
