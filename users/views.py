0aaafc2330445641066d355956f5874a6bab3d5f
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-11-26 15:14:19 +0100

    Validate inserted details

--- users/views.py
+++ users/views.py
@@ -23,6 +23,11 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
