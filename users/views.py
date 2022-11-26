f47facfc5de261dd9c9d97533b75024ee2f48a63
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-11-26 15:59:03 +0100

    Resize image by overiding the save method

--- users/views.py
+++ users/views.py
@@ -27,11 +27,13 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            
+            messages.success(request, f'Your account has been Updated')
+            return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-
+     
     context = {
         'u_form': u_form,
         'p_form': p_form
